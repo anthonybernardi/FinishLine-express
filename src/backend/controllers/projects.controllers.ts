be38af997a69edd27dfb0634be0300950194c7d4
anthonybernardi <anthony.bernardi09@gmail.com>
2022-07-30 23:10:57 -0400

    add getSingleProject

--- src/backend/controllers/projects.controllers.ts
+++ src/backend/controllers/projects.controllers.ts
@@ -1,9 +1,35 @@
 import { PrismaClient } from '@prisma/client';
-import { manyRelationArgs, projectTransformer } from '../utils/projects.utils';
+import { isProject, validateWBS, WbsNumber } from 'shared';
+import { manyRelationArgs, projectTransformer, uniqueRelationArgs } from '../utils/projects.utils';
 
 const prisma = new PrismaClient();
 
 export const getAllProjects = async (_req: any, res: any) => {
   const projects = await prisma.project.findMany(manyRelationArgs);
   res.status(200).json(projects.map(projectTransformer));
+};
+
+export const getSingleProject = async (req: any, res: any) => {
+  const parsedWbs: WbsNumber = validateWBS(req.params.wbsNum);
+
+  if (!isProject(parsedWbs)) {
+    return res.status(404).json({ message: `${req.params.wbsNum} is not a valid project WBS #!` });
+  }
+
+  const wbsEle = await prisma.wBS_Element.findUnique({
+    where: {
+      wbsNumber: {
+        carNumber: parsedWbs.carNumber,
+        projectNumber: parsedWbs.projectNumber,
+        workPackageNumber: parsedWbs.workPackageNumber
+      }
+    },
+    ...uniqueRelationArgs
+  });
+
+  if (wbsEle === null) {
+    return res.status(404).json({ message: `project ${req.params.wbsNum} not found!` });
+  }
+
+  return res.status(200).json(projectTransformer(wbsEle));
 };
--- src/backend/routes/projects.routes.ts
+++ src/backend/routes/projects.routes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getAllProjects } from '../controllers/projects.controllers';
+import { getAllProjects, getSingleProject } from '../controllers/projects.controllers';
 
 const projectRouter = express.Router();
 
 projectRouter.get('/', getAllProjects);
+projectRouter.get('/:wbsNum', getSingleProject);
 
 export default projectRouter;

62c274c1678bdc9f73d849b3f70ff201a084b3c9
Anthony Bernardi <bernardi.an@husky.neu.edu>
2022-07-31 15:32:18 -0400

    moving backend around and messing with build stuff

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import userRouter from './routes/users.routes';
-import projectRouter from './routes/projects.routes';
+import userRouter from './src/routes/users.routes';
+import projectRouter from './src/routes/projects.routes';
 
 const app = express();
 const port = process.env.PORT || 3001;
